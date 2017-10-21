227b240b5cfa0b9350fe69d80c1edeef6b0e9334
Coubiac <benoit.grisot@gmail.com>
2017-10-21 10:41:30 +0200

    Correction JS

--- web/js/datepicker.js
+++ web/js/datepicker.js
@@ -91,63 +91,53 @@ $(document).ready(function () {
             firstDay: 1,
             closeOnSelect: false,
             onSet: function (context) {
-                var selectedTimestamp = context.select / 1000;
+                var selectedTimestamp = parseInt(context.select / 1000);
                 var route = Routing.generate('countavailabletickets', {'timestamp': selectedTimestamp});
-                console.log(context.select);
                 $.ajax({
                     async: true,
                     type: "GET",
                     url: route,
                     dataType: "json",
                     success: function (response) {
-                        var selectedTimestamp = parseInt(context.select / 1000);
-                        var route = Routing.generate('countavailabletickets', {'timestamp': selectedTimestamp});
-                        $.ajax({
-                            async: true,
-                            type: "GET",
-                            url: route,
-                            dataType: "json",
-                            success: function (response) {
-                                maxTickets = response;
-                                $('ul.tickets li').remove();
-                                $('.remove_ticket_link').remove();
-                                if(response < 1){
-                                    minTickets = 0;
-                                }
-                                if(response < 10){
-                                    Materialize.toast('Warning ! there are only ' + response + ' tickets left on this date !', 4000, 'deep-orange darken-4 rounded');
-                                }
-                                if (response > 0) {
-                                    // Get the ul that holds the collection of tickets
-                                    $collectionHolder = $('ul.tickets');
-                                    // add a delete link to all of the existing tag form li elements
-                                    $collectionHolder.find('li').each(function () {
-                                        addTicketFormDeleteLink($(this));
-                                    });
-                                    // add the "add a ticket" anchor and li to the tickets ul
-                                    $collectionHolder.append($newLinkLi);
-                                    // count the current form inputs we have (e.g. 2), use that as the new
-                                    // index when inserting a new item (e.g. 2)
-                                    $collectionHolder.data('index', $collectionHolder.find(':input').length);
-                                    $addTicketLink.on('click', function (e) {
-                                        // prevent the link from creating a "#" on the URL
-                                        e.preventDefault();
-                                        // add a new ticket form (see next code block)
-                                        addTicketForm($collectionHolder, $newLinkLi);
-                                    });
-                                }
-                                dateOfVisit = new Date(parseInt(context.select));
-                                if (transformDate(dateOfVisit) === transformDate(now) && hour >= 14 && hour < 24){
-                                    fullday.remove();
-                                    $('select').material_select();
-                                }
-                                else{
-                                    fullday.insertBefore(halfDay);
-                                    $('select').material_select();
-                                }
-                            }
-                        });
+                        maxTickets = response;
+                        $('ul.tickets li').remove();
+                        $('.remove_ticket_link').remove();
+                        if(response < 1){
+                            minTickets = 0;
+                        }
+                        if(response < 10){
+                            Materialize.toast('Attention ! il ne reste que ' + response + ' tickets à cette date !', 4000, 'deep-orange darken-4 rounded');
+                        }
+                        if (response > 0) {
+                            // Get the ul that holds the collection of tickets
+                            $collectionHolder = $('ul.tickets');
+                            // add a delete link to all of the existing tag form li elements
+                            $collectionHolder.find('li').each(function () {
+                                addTicketFormDeleteLink($(this));
+                            });
+                            // add the "add a ticket" anchor and li to the tickets ul
+                            $collectionHolder.append($newLinkLi);
+                            // count the current form inputs we have (e.g. 2), use that as the new
+                            // index when inserting a new item (e.g. 2)
+                            $collectionHolder.data('index', $collectionHolder.find(':input').length);
+                            $addTicketLink.on('click', function (e) {
+                                // prevent the link from creating a "#" on the URL
+                                e.preventDefault();
+                                // add a new ticket form (see next code block)
+                                addTicketForm($collectionHolder, $newLinkLi);
+                            });
+                        }
+                        dateOfVisit = new Date(parseInt(context.select));
+                        if (transformDate(dateOfVisit) === transformDate(now) && hour >= 14 && hour < 24){
+                            fullday.remove();
+                            $('select').material_select();
+                        }
+                        else{
+                            fullday.insertBefore(halfDay);
+                            $('select').material_select();
+                        }
                     }
+                });
             },
             disable: hollidays
         });
