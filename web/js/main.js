17b8abedf93c4362993aca628474262936f52791
Coubiac <benoit.grisot@gmail.com>
2017-10-22 23:59:26 +0200

    Ajout d'une modale pour afficher les tarifs

--- web/js/main.js
+++ web/js/main.js
@@ -2,8 +2,10 @@ $(document).ready(function () {
 
     //-------------------------------------------
     //Material Design
+    $(".button-collapse").sideNav();
     $('.parallax').parallax();
     $('select').material_select();
+    $('.modal').modal();
 
 
 
